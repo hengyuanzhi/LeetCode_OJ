0ddd3814203691c1ae02115b86b275dc9cd5e657
Neil <zhangsm_neil@163.com>
2015-07-15 18:33:53 +0800

    建立头文件，定义所需的结构体

---
+++ Git_LeetCode/defstruct.h
@@ -0,0 +1,14 @@
+#ifndef DEFSTRUCT_H_
+#define DEFSTRUCT_H_
+
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+#endif
