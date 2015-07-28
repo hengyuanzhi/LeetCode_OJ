6b6c46c198bad82aec258dcf64ac8edfcf65fb55
Neil <zhangsm_neil@163.com>
2015-07-28 14:19:13 +0800

    7月28日提交

--- Git_LeetCode/69_Sqrt_x.cpp
+++ Git_LeetCode/69_Sqrt_x.cpp
@@ -5,3 +5,20 @@
 #include <algorithm>
 #include"defstruct.h"
 using namespace std;
+
+class Solution {
+public:
+	int mySqrt(int x)
+	{
+		if (x == 0)return 0;
+		double x1, x2;
+		x2 = x;
+
+		do
+		{
+			x1 = x2;
+			x2 = 0.5*x1 + 0.5*x / x1;
+		} while (abs(x1 - x2) > 0.0001);
+		return int(x2);
+	}
+};
